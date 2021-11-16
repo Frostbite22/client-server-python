68e2bfb727ef4d6ec4978e9005eb8875165e71f3
Frostbite22 <landoulsiferes@outlook.com>
2021-11-16 08:48:28 -0500

    multiple conn

--- Part 2 : finger/fingerd.py
+++ Part 2 : finger/fingerd.py
@@ -7,15 +7,16 @@ PORT = 9797    # Port to listen on (non-privileged ports are > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            print(data.decode())
-            if not data:
-                break
-            process = subprocess.Popen(['finger', data.decode()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = process.communicate()
-            conn.sendall(out)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                print(data.decode())
+                if not data:
+                    break
+                process = subprocess.Popen(['finger', data.decode()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, err = process.communicate()
+                conn.sendall(out)
 
             
